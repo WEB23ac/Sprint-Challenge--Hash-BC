251644660049e9e63b8c074bbbe224e5eaad5049
Aaron Clark <aaronpclark92@gmail.com>
2020-03-13 10:46:01 -0500

    feat: ex1 passes

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,11 +8,25 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
     """
     YOUR CODE HERE
     """
+    
 
+# * adds each weight entry to HT
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+
+
+    for j in range(length):
+        check_val = limit - weights[j]
+        matched_idx = hash_table_retrieve(ht, check_val)
+
+        if matched_idx is not None:
+            if matched_idx > j:
+                return (matched_idx, j)
+            else:
+                return  (j, matched_idx)
     return None
 
 
